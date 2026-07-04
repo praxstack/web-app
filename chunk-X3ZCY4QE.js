4c6f46e023a51d7d94ce6fc997e3c5963c5821e7
praxstack <prakhar.2019ca66@gmail.com>
2026-07-04 05:42:40 +0000

    Deploying to gh-pages from @ praxstack/web-app@63129a71f2368de812a0a464e8bbd5953a2b03cf 🚀

---
+++ chunk-X3ZCY4QE.js
@@ -0,0 +1,1 @@
+import{a as i}from"./chunk-ECVL5WV3.js";import{ea as r,ja as e}from"./chunk-BIQ5BY46.js";var m=(()=>{class t{constructor(){this.organizationService=e(i)}resolve(){return this.organizationService.getOffices()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac})}}return t})();export{m as a};
