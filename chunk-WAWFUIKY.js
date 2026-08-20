861178daf9b21a267c29e11d79c3b5f14ac07302
praxstack <73683289+praxstack@users.noreply.github.com>
2026-08-20 01:23:20 +0000

    Deploying to gh-pages from @ praxstack/web-app@e81a43e0e06933053439a4f44969ea9bc2956c27 🚀

--- chunk-WAWFUIKY.js
+++
@@ -1,1 +0,0 @@
-import{Qa as r}from"./chunk-N7MV6IQW.js";import{W as c,_ as e}from"./chunk-ATMBOCJV.js";var f=(()=>{class t{constructor(){this.accountingService=e(r)}resolve(){return this.accountingService.getLoanProducts()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=c({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
--- chunk-XNQUHJ37.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./chunk-EKNC2XKU.js";import{W as r,_ as e}from"./chunk-ATMBOCJV.js";var m=(()=>{class t{constructor(){this.organizationService=e(i)}resolve(){return this.organizationService.getCurrencies()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac})}}return t})();export{m as a};
